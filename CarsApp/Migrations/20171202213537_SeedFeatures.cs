db47b32ca04244223f17fc808266ce709ad48e3b
Serhii Prostakov <s.prostakov@yandex.com>
2017-12-02 23:42:20 +0200

    Added SeedFeatures migration

---
+++ CarsApp/Migrations/20171202213537_SeedFeatures.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CarsApp.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Anti-lock braking system ABS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Forward-collision warning (FCW)')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Automatic emergency braking (AEB)')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name IN ('Anti-lock braking system ABS', 'Forward-collision warning (FCW)', 'Automatic emergency braking (AEB)')");
+        }
+    }
+}
